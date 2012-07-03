d31b68f206e9d1ada036a69210ac7e85ecaa46fd
Joshua Johanan <josh359@gmail.com>
2012-07-03 00:02:19 -0400

    node

--- app.js
+++ app.js
@@ -16,8 +16,8 @@ function handler (req, res) {
   
 }
 
-usernameExists = function(event, username){
-	client.get(event+':users:'+username, function(err, data){
+usernameExists = function(area, username){
+	client.get(area+':users:'+username, function(err, data){
 		if(data == null){
 			return false;
 		}else{
@@ -26,23 +26,35 @@ usernameExists = function(event, username){
 	});
 };
 
+var User = function(username, img, area, socketid){
+	this.username = username;
+	this.img = img;
+	this.area = area;
+	this.socketid = socketid;
+};
+
 var users = io.of('/users').on('connection', function (socket) {
-	socket.set('username', socket.id);
+	//socket.set('username', socket.id);
+	var user;
 	
-	socket.on('add', function(username, img){
-	
-	if(!usernameExists('test', username)){
-		setUser(username, img, 7200);
-		socket.set('username', username);
-		socket.set('img', img);
+	socket.on('add', function(username, img, area){
+	console.log('area: ' + area);
+	if(!usernameExists(area, username)){
+		setUser(username, img, socket.id, area, 7200);
+		var test = new User(username, img, area, socket.id);
+		console.log(test);
+		user = test;
+		socket.set('user', test);
+		//socket.set('img', img);
 		console.log(socket.id + ' : ' + username);
 
 	}
 		//add the user to redis here
 		//put all the users here
 		
-		socket.broadcast.emit('users', {username: username, img: img});
-		socket.emit('users', {username: username, img: img, me: true});
+		//socket.broadcast.emit('users', {username: username, img: img});
+		//socket.emit('users', {username: username, img: img, me: true});
+		io.of('/users').sockets[user.socketid].emit('test', 'hello');
 		});
 	
 	
@@ -71,19 +83,23 @@ var users = io.of('/users').on('connection', function (socket) {
 		});
 	});
 	
-	socket.on('vote', function(fs){
-		socket.get('username', function(err, user){
-			socket.get('img', function(err, img){
-				console.log(user + ' voted for : ' + JSON.stringify(fs));
-				setVote(user, fs, 7200);
-				socket.broadcast.emit('vote', {username: user, img: img, fs: fs});
-			});
+	socket.on('addVote', function(fs){
+		socket.get('user', function(err, user){
+			//socket.get('img', function(err, img){
+				console.log(user.username + ' voted for : ' + JSON.stringify(fs));
+				setVote(user.username, user.area, fs, 7200);
+				//need to rewrite the broadcast
+				//socket.broadcast.emit('vote', {username: user.username, img: user.img, fs: fs});
+				areaBroadcast(user.username, user.area, io.of('/users'), 'vote', {username: user.username, img: user.img, fs: fs});
+				socket.emit('vote', {username: user.username, img: user.img, fs: fs});
+			//});
 		});
 	});
 	
 	socket.on('getVotes', function(){
-		checkUserSet('test:votes', function(){
-			client.smembers('test:votes', function(err, votes){
+		var area = user.area;
+		checkUserSet(area+':votes', function(){
+			client.smembers(area+':votes', function(err, votes){
 				if(votes != null){
 					votes.forEach(function(key){
 						client.get(key, function(err, username){
@@ -100,32 +116,41 @@ var users = io.of('/users').on('connection', function (socket) {
 	});
 	socket.on('disconnect', function(client){
 		//console.log(socket.get('username', function(err, user){removeUser(user);}) + ' disconnected');
+		if(user != null){
+			removeUser(user.username, user.area, function(){
+				console.log('user left:' + user.username );
+			});
+		}
 	});
 	
 });
 
-function setVote(username, fs, expire){
-	client.set('test:users:' + username + ':vote', JSON.stringify(fs), redis.print);
-	client.sadd('test:votes', 'test:users:' + username, redis.print);
+function setVote(username, area, fs, expire){
+	client.set(area+':users:' + username + ':vote', JSON.stringify(fs), redis.print);
+	client.sadd(area+':votes', area+':users:' + username, redis.print);
 	//set a timer
-	client.set('test:votes:timer', expire, redis.print);
-	client.expire('test:votes:timer', expire, redis.print);
+	client.set(area+':votes:timer', expire, redis.print);
+	client.expire(area+':votes:timer', expire, redis.print);
 };
 
-function setUser(username, img, expire){
-	client.set('test:users:' + username, username, redis.print);
-	client.expire('test:users:' + username, expire, redis.print);
-	client.set('test:users:' + username + ':img', img, redis.print);
-	client.expire('test:users:' + username + ':img', expire, redis.print);
-	client.set('test:users:timer', expire, redis.print);
-	client.expire('test:users:timer', expire, redis.print);
-	client.sadd('test:users', 'test:users:' + username, redis.print);
+function setUser(username, img, socketid, area, expire){
+	client.set(area+':users:' + username, username, redis.print);
+	client.expire(area+':users:' + username, expire, redis.print);
+	client.set(area+':users:' + username + ':img', img, redis.print);
+	client.expire(area+':users:' + username + ':img', expire, redis.print);
+	client.set(area+':users:' + username + ':socketid', socketid, redis.print);
+	client.expire(area+':users:' + username + ':socketid', expire, redis.print);
+	client.set(area+':users:timer', expire, redis.print);
+	client.expire(area+':users:timer', expire, redis.print);
+	client.sadd(area+':users', area+':users:' + username, redis.print);
 };
 
-function removeUser(username){
-	client.del('test:users:' + username);
-	client.del('test:users:' + username + ':img');
-	client.srem('test:users', 'test:users:' + username);
+function removeUser(username, area, callback){
+	client.del(area+':users:' + username);
+	client.del(area+':users:' + username + ':img');
+	client.del(area+':users:' + username + ':socketid');
+	client.srem(area+':users', area+':users:' + username);
+	callback();
 };
 
 function checkUserSet(set, callback){
@@ -134,5 +159,23 @@ function checkUserSet(set, callback){
 			client.del(set);
 		}
 		 callback();
+	});
+};
+
+function areaBroadcast(username, area, connect, name, cast){
+	client.smembers(area+':users', function(err, members){
+		if(members != null){
+			members.forEach(function(areaUser){
+				console.log(areaUser);
+				console.log(area+':users:' + areaUser + ':socketid');
+				if(area+':users:'+username != areaUser){
+					client.get(area+':users:' + areaUser + ':socketid', function(err, socks){
+						if(socks != null)
+							console.log(socks);
+							//connect.sockets[socks].emit(name, cast);
+					});
+				}
+			});
+		}
 	});
 };
